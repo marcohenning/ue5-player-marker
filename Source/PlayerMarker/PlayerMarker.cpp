18fd2405f06d387b0bb51a8ac8fd09348c0b7eff
Marco Henning <marcohnngpersonal@gmail.com>
2024-05-18 09:45:07 +0200

    Initial commit

---
+++ Source/PlayerMarker/PlayerMarker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlayerMarker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlayerMarker, "PlayerMarker" );
